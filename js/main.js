b2c769b45641cf5124b391eaed9eb5726f6712ed
Sergey Gribanov <sergeygribanov@192-168-0-106.local>
2018-07-31 23:49:12 +0300

    Main block added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
 
+	// Добавление последних дат в фильтр
+
 	function formatDate(date) {
 	  var dd = date.getDate();
 	  if (dd < 10) dd = '0' + dd;
@@ -20,6 +22,8 @@ jQuery(document).ready(function ($) {
 		$('.filter-date').append('<a class="filter-item-date" href="#">' + formatDate(temp) + '</a>');
 	}
 
+		// Выбор валюты для ставки в купоне
+
 	var currency = ['fa-ruble-sign', 'fa-dollar-sign', 'fa-euro-sign'];
 
 	$('.currency-item').click(function() {
@@ -38,12 +42,16 @@ jQuery(document).ready(function ($) {
 		}
 	})
 
+	// Проверка вводимого значения в поле суммы для ставки
+
 	var v = "";
 	$('#bet-sum').on('input', function () {
 		$(this).val($(this).val().replace(/[^\d]/g, ''));
 		temp = $(this).val() * parseFloat($('#koef-num').html());
 		$('#win-num').html(temp.toFixed(2));
 	});
+
+	// Анимация меню выбора режима ставки в купоне
 
 	$('#coupon-toggle-1').click(function() {
 		if ($(this).hasClass('toggle-selected') == false) {
@@ -57,5 +65,49 @@ jQuery(document).ready(function ($) {
 			$('#coupon-toggle-1').removeClass('toggle-selected');
 		}
 	});
+
+	// Открытие списков событий
+
+	$('.event-head').each(function () {
+		if ($(this).hasClass('event-selected-item')) {
+			$(this).css('border-radius', '5px 5px 0 0');
+			$(this).find('i.fa-angle-down').css('transform', 'rotate(-180deg) translateY(2px)');
+			$(this).parent().children('div.event-body').show();
+		}
+	});
+
+	$('.event-head').click(function(){
+		var event = $(this).parent().children('div.event-body');
+		var arrow = $(this).find('i.fa-angle-down');
+
+		if (event.is(':hidden')) {
+			arrow.css('transform', 'rotate(-180deg) translateY(2px)');
+			$(this).addClass('event-selected-item');
+			$(this).css('border-radius', '5px 5px 0 0');
+		} else {
+			arrow.css('transform', 'rotate(0deg) translateY(0px)');
+			$(this).removeClass('event-selected-item');
+			$(this).css('border-radius', '5px');
+		}
+	  event.slideToggle('normal');
+	});
+
+	// Выбор ставки для события
+
+	$('.event-koef').on("click", function(event){
+	 	var target = $(event.target);
+		var targetBlock = $(this).parents().closest('tr');
+		var koefs = targetBlock.find('.event-koef-red');
+		if (koefs.length == 0) {
+			target.toggleClass('event-koef-red');
+		} else {
+			if (target.hasClass('event-koef-red')) {
+				target.toggleClass('event-koef-red');
+				return;
+			}
+			koefs.removeClass('event-koef-red');
+			target.toggleClass('event-koef-red');
+		};
+ 	});
 
 });
